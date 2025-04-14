4f9be09a34e27906deea713969214a8416731f91
Thomas Berlier <thomas.berlier@necko.tech>
2025-04-14 19:50:37 +0200

    nav and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './header/header.component';
+import {NavigationComponent} from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavigationComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
-})
+  styleUrl: './app.component.scss',})
 export class AppComponent {
   title = 'demo-tec';
 }
