967e3298839e97fb3424612de2ae1d4a40229f4c
Thomas Berlier <berlier.thomas@gmail.com>
2025-04-15 16:54:17 +0200

    navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,34 +1,42 @@
 import { Component, signal } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 
-// function
-
 export class NavigationComponent {
   menuItems = [
-    {name: 'Se déplacer', subMenuItems: [
-      "Itinéraires",
-      "Horaires par ligne/arrêt",
-      "A proximité",
-      "Vers l'aéropo",
-      "Info trafic",
-      "Plan intempéries",
-      "Lignes express",
-    ]},
-    {name: "Titres et tarifs", subMenuItems: []},
-    {name: "Besoin d'aide", subMenuItems: []},
-    {name: "Jobs", subMenuItems: []},
-  ]
+    {
+      name: 'Se déplacer', subMenuItems: [
+        'Itinéraires',
+        'Horaires par ligne/arrêt',
+        'A proximité',
+        'Vers l\'aéropo',
+        'Info trafic',
+        'Plan intempéries',
+        'Lignes express'
+      ]
+    },
+    { name: 'Titres et tarifs', subMenuItems: [] },
+    { name: 'Besoin d\'aide', subMenuItems: [] },
+    { name: 'Jobs', subMenuItems: [] }
+  ];
 
-  menu = signal(false)
+  menu = signal(false);
 
   openMenu() {
-  this.menu.set(!this.menu());
-  console.log(this.menu());
-}
+    this.menu.set(!this.menu());
+  }
+
+  subItemsState = signal(false);
+
+  showSubItems() {
+    this.subItemsState.set(!this.subItemsState());
+  }
 }
