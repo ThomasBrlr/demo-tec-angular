d65c86a166e485732560df33c217eaeabdae944a
Thomas Berlier <berlier.thomas@gmail.com>
2025-04-15 13:02:51 +0200

    navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -6,6 +6,29 @@ import { Component } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
 
+// function
+
+export class NavigationComponent {
+  menuItems = [
+    {name: 'Se déplacer', subMenuItems: [
+      "Itinéraires",
+      "Horaires par ligne/arrêt",
+      "A proximité",
+      "Vers l'aéropo",
+      "Info trafic",
+      "Plan intempéries",
+      "Lignes express",
+    ]},
+    {name: "Titres et tarifs", subMenuItems: []},
+    {name: "Besoin d'aide", subMenuItems: []},
+    {name: "Jobs", subMenuItems: []},
+  ]
+
+  menu = signal(false)
+
+  openMenu() {
+  this.menu.set(!this.menu());
+  console.log(this.menu());
+}
 }
